77feb88a5b72e474927e0f5810562b78694f47b2
curry <curry.zheng@ceosoftcenters.com>
2018-04-08 18:16:11 +0800

    沉浸式状态栏
    引用了https://github.com/gyf-dev/ImmersionBar库(还可以参考https://github.com/jgilfelt/SystemBarTint);

--- app/src/main/java/com/curry/android/android_collection/activity/MainActivity.java
+++ app/src/main/java/com/curry/android/android_collection/activity/MainActivity.java
@@ -4,12 +4,18 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.curry.android.android_collection.R;
+import com.curry.android.android_collection.base.BaseActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_main;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
     }
+
 }
--- app/src/main/java/com/curry/android/android_collection/base/BaseActivity.java
+++ app/src/main/java/com/curry/android/android_collection/base/BaseActivity.java
@@ -1,12 +1,73 @@
 package com.curry.android.android_collection.base;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import com.curry.android.android_collection.R;
+import com.gyf.barlibrary.ImmersionBar;
 
 /**
  * @author curry
  * created at 2018/4/3 11:54
  * @desc OK
  */
-public class BaseActivity extends AppCompatActivity {
+public abstract class BaseActivity extends AppCompatActivity {
+    private ImmersionBar immersionBar;
 
+    /**
+     * this activity layout res
+     * 设置layout布局,在子类重写该方法.
+     *
+     * @return res layout xml id
+     */
+    protected abstract int getLayoutId();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.setContentView(R.layout.base_layout);
+        setContentView();
+        immersionBar = ImmersionBar.with(this);
+        immersionBar.statusBarView(R.id.top_view)
+                .statusBarDarkFont(true,0.2f)
+                .statusBarColor(R.color.white).init();
+    }
+
+    /**
+     * 将布局添加到base_layout
+     */
+    public void setContentView() {
+        LinearLayout baseLayout = findViewById(R.id.base_layout);
+        if (baseLayout == null) return;
+        baseLayout.addView(View.inflate(this, getLayoutId(), null),
+                new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT));
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
+        if (getSupportActionBar() != null) {
+            // Enable the Up button
+            getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (immersionBar != null) {
+            immersionBar.destroy();
+        }
+    }
 }
+
