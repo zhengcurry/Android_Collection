ddae27945ee5b4e641178fa96c70e06e0bba1dc5
curry <AYI0000AYI@gmail.com>
2018-11-20 09:21:55 +0800

    build

--- app/src/main/java/com/curry/android/android_collection/activity/MainActivity.java
+++ app/src/main/java/com/curry/android/android_collection/activity/MainActivity.java
@@ -1,11 +1,7 @@
 package com.curry.android.android_collection.activity;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.res.Configuration;
-import android.content.res.Resources;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.TextView;
 
@@ -17,8 +13,6 @@ import com.curry.android.android_collection.base.BaseActivity;
 import com.example.makejarlibrary.MakeJar;
 
 import org.w3c.dom.Text;
-
-import java.util.Locale;
 
 import butterknife.OnClick;
 
@@ -38,7 +32,7 @@ public class MainActivity extends BaseActivity {
     }
 
     @OnClick({R.id.btn_test_immersion, R.id.btn_line_chart, R.id.btn_bar_chart,
-            R.id.btn_combined_chart, R.id.btn_view, R.id.btn_change})
+            R.id.btn_combined_chart, R.id.btn_view})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btn_test_immersion:   //沉浸式状态栏
@@ -56,35 +50,7 @@ public class MainActivity extends BaseActivity {
             case R.id.btn_view:             //自定义View
                 startActivity(new Intent(this, CanvasActivity.class));
                 break;
-            case R.id.btn_change:             //自定义View
-//                changeLanguage();
-                saveLanguage();
-                recreate();
-                break;
         }
     }
 
-    private void changeLanguage() {
-
-        Resources resources = getResources();
-        DisplayMetrics dm = resources.getDisplayMetrics();
-        Configuration config = resources.getConfiguration();
-        // 应用用户选择语言
-        config.locale = Locale.ENGLISH;
-        resources.updateConfiguration(config, dm);
-    }
-
-    private void saveLanguage() {
-        SharedPreferences sharedPreferences =
-                getSharedPreferences("curry", MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        if (sharedPreferences.getString("language", "cn").equals("cn")) {
-            editor.putString("language", "en");
-        } else {
-            editor.putString("language", "cn");
-        }
-        editor.commit();
-    }
 }
-
-
