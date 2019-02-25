8f4bb35d377c2805a3d705837023fe3a366ede42
curry <curry.zheng@ceosoftcenters.com>
2019-02-25 23:21:43 +0800

    屏幕适配-今日头条方案

---
+++ curry_base/src/main/java/com/curry/basic/utils/ExtendDisplayUtil.java
@@ -0,0 +1,89 @@
+package com.curry.basic.utils;
+
+import android.app.Activity;
+import android.app.Application;
+import android.content.ComponentCallbacks;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.support.annotation.NonNull;
+import android.util.DisplayMetrics;
+
+/**
+ * 在今日头条方案基础上的 扩展
+ * Android适配：修改设备密度
+ */
+public class ExtendDisplayUtil {
+
+    /**
+     * Adapt the screen for vertical slide.
+     *
+     * @param activity        The activity.
+     * @param designWidthInPx The size of design diagram's width, in pixel.
+     */
+    public static void adaptScreen4VerticalSlide(final Activity activity,
+                                                 final int designWidthInPx) {
+        adaptScreen(activity, designWidthInPx, true);
+    }
+
+    /**
+     * Adapt the screen for horizontal slide.
+     *
+     * @param activity         The activity.
+     * @param designHeightInPx The size of design diagram's height, in pixel.
+     */
+    public static void adaptScreen4HorizontalSlide(final Activity activity,
+                                                   final int designHeightInPx) {
+        adaptScreen(activity, designHeightInPx, false);
+    }
+
+    /**
+     * Reference from: https://mp.weixin.qq.com/s/d9QCoBP6kV9VSWvVldVVwA
+     */
+    private static void adaptScreen(final Activity activity,
+                                    final int sizeInPx,
+                                    final boolean isVerticalSlide) {
+      /*  final DisplayMetrics systemDm = Resources.getSystem().getDisplayMetrics();
+        final DisplayMetrics appDm = Utils.getApp().getResources().getDisplayMetrics();
+        final DisplayMetrics activityDm = activity.getResources().getDisplayMetrics();
+        if (isVerticalSlide) {
+            activityDm.density = activityDm.widthPixels / (float) sizeInPx;
+        } else {
+            activityDm.density = activityDm.heightPixels / (float) sizeInPx;
+        }
+        activityDm.scaledDensity = activityDm.density * (systemDm.scaledDensity / systemDm.dens
+        activityDm.densityDpi = (int) (160 * activityDm.density);
+        appDm.density = activityDm.density;
+        appDm.scaledDensity = activityDm.scaledDensity;
+        appDm.densityDpi = activityDm.densityDpi;*/
+    }
+
+    /**
+     * Cancel adapt the screen.
+     *
+     * @param activity The activity.
+     */
+    public static void cancelAdaptScreen(final Activity activity) {
+        /*final DisplayMetrics systemDm = Resources.getSystem().getDisplayMetrics();
+        final DisplayMetrics appDm = Utils.getApp().getResources().getDisplayMetrics();
+        final DisplayMetrics activityDm = activity.getResources().getDisplayMetrics();
+        activityDm.density = systemDm.density;
+        activityDm.scaledDensity = systemDm.scaledDensity;
+        activityDm.densityDpi = systemDm.densityDpi;
+        appDm.density = systemDm.density;
+        appDm.scaledDensity = systemDm.scaledDensity;
+        appDm.densityDpi = systemDm.densityDpi;*/
+    }
+
+    /**
+     * Return whether adapt screen.
+     *
+     * @return {@code true}: yes<br>{@code false}: no
+     */
+    public static boolean isAdaptScreen() {
+        /*final DisplayMetrics systemDm = Resources.getSystem().getDisplayMetrics();
+        final DisplayMetrics appDm = Utils.getApp().getResources().getDisplayMetrics();
+        return systemDm.density != appDm.density;*/
+        return false;
+    }
+
+}
